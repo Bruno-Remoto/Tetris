19f87e252267ced023c802a977644c118b6966b0
Bruno-Remoto <79065642+Bruno-Remoto@users.noreply.github.com>
2021-04-13 09:02:20 -0300

    Update painel.js

--- js/painel.js
+++ js/painel.js
@@ -1,4 +1,16 @@
 class Painel {
+    ctx;
+    ctxNext;
+    grid;
+    peca;
+    next;
+    idRequisicao;
+    tempo;
+    init() {
+        this.ctx.canvas.width = Coluna * Tam_Bloco;
+        this.ctx.canvas.height = Linha * Tam_Bloco;
+        
+        this.ctx.scale(Tam_Bloco, Tam_Bloco);
     reset() {
         this.grid = this.getPainelVazio();
     }
@@ -15,6 +27,20 @@ class Painel {
                 let x = p.x + dx;
                 let y = p.y + dy;
                 return (this.grid[y] && this.grid[y][x] === 0 || (x >= 0 && x < Coluna && y <= Linha));
+            })
+        })
+    }
+    desenho() {
+        this.peca.desenho();
+        this.desenhoPainel();
+    }
+    desenhoPainel() {
+        this.grid.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if(value > 0) {
+                    this.ctx.fillStyle = Cores[value];
+                    this.ctx.fillRext(x, y, 1, 1);
+                }
             })
         })
     }
